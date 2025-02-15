329b8089b52ff8b2637a8cdd23ea18df6d34458b
bufan <1395470761@qq.com>
2025-02-15 12:40:25 +0800

    upload_data

--- data/Singing_Detection/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start "MyExperimentWindow" cmd /c "experiment.cmd & exit"
-timeout /t 40
-taskkill /fi "WINDOWTITLE eq MyExperimentWindow" /f
-start "MyExperimentWindow1" cmd /c "run.bat & exit"
